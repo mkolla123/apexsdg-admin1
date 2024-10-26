8e06650dde3f4a61ea1944da386c8147c51659ca
mkolla123 <65868597+mkolla123@users.noreply.github.com>
2024-10-26 22:02:51 +0530

    Update AdminReview.py

--- AdminReview.py
+++ AdminReview.py
@@ -10,10 +10,10 @@ import pandas as pd
 #from sqlalchemy.ext.declarative import declarative_base
 
 db_config = {
-    'host': '127.0.0.1',
-    'user': 'test1',
-    'password': 'test1',
-    'database': 'apexsdg',
+    'host': 'srv1118.hstgr.io',
+    'user': 'u829120591_bV3z1',
+    'password': 'u829120591_bV3z1U',
+    'database': 'u829120591_4XuUf',
 }
 
 conn = mysql.connector.connect(**db_config)
